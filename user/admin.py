7a7ba1d29c6c00694e6c3a36989ed9c38e9a5161
Monirul Islam <mr.monirul.dev@gmail.com>
2025-08-14 16:57:21 +0600

    decorate admin panel

--- user/admin.py
+++ user/admin.py
@@ -1,3 +1,57 @@
 from django.contrib import admin
+from user.models import CustomUser
 
 # Register your models here.
+
+
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = [
+        "username",
+        "first_name",
+        "last_name",
+        "balance",
+        "email",
+        "is_active",
+    ]
+
+    fieldsets = (
+        (
+            None,
+            {
+                "fields": ("username", "password"),
+            },
+        ),
+        (
+            "Personal Info:",
+            {
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "email",
+                    "balance",
+                )
+            },
+        ),
+        (
+            "Permissions:",
+            {
+                "fields": (
+                    "is_active",
+                    "is_staff",
+                    "is_superuser",
+                    "groups",
+                    "user_permissions",
+                )
+            },
+        ),
+        (
+            "Important Dates:",
+            {
+                "fields": (
+                    "last_login",
+                    "date_joined",
+                )
+            },
+        ),
+    )
